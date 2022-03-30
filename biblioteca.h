43818639f52c297c2da6cfea16f2c57b42ade448
João Vitor Fonseca <61283617+Joaovitron999@users.noreply.github.com>
2022-03-30 02:49:03 +0000

    Final

--- biblioteca.h
+++ biblioteca.h
@@ -1,8 +1,40 @@
+#include <fstream>
+#include "biblioteca.cpp"
+#include <cstdlib>
+
+using namespace std;
+
 #ifndef BIBLIOTECA_H
 #define BIBLIOTECA_H
 
-//Exemplo func
+//Inicio
 
-int fmenu();
+struct Carro;
+
+class No;
+
+class Lista;
+
+class Pilha;
+
+class Fila;
+
+int menu();
+
+void exibeCarro(Carro c1);
+
+void exibeLista(No* cabeca);
+
+void removeEspecifico(Carro c, Lista* lista);
+
+void removeColetivo(Carro c, Lista* lista);
+
+void buscaPilha(Carro c1,Carro c2,No* cabecaLista,Pilha* pilha);
+
+void buscaFila(Carro c1,Carro c2,No* cabecaLista,Fila* fila);
+
+void lerParaLista(Lista *lista);
+
+void incluirVeiculo(Lista* lista);
+
 #endif
-
